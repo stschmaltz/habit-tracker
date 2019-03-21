856f7a471d5409cc309b80448f90d74d126df1fb
Shane Schmaltz <stschmaltz@gmail.com>
2019-03-20 19:34:45 -0600

    add the start of the add habit dialog and add ember material library to project

--- app/components/add-new-habit-dialog/component.js
+++ app/components/add-new-habit-dialog/component.js
@@ -1,9 +1,15 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
+  habitName: "",
+  frequency: 0,
+
   actions: {
+    addHabit(habit) {
+      console.log(habit);
+    },
     cancel() {
       this.onClose();
-    },
-  },
+    }
+  }
 });
