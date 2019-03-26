32c85a53e8b3c66f4fbd5915ff9ec1138c39a9a9
Shane Schmaltz <stschmaltz@gmail.com>
2019-03-25 21:38:39 -0600

    figured out how to hook up form handlers

--- app/components/add-new-habit-dialog/component.js
+++ app/components/add-new-habit-dialog/component.js
@@ -1,12 +1,40 @@
 import Component from "@ember/component";
 
 export default Component.extend({
-  habitName: "",
-  frequency: 0,
+  init() {
+    this._super(...arguments);
 
+    this.frequencies = this.frequencies || [1, 2, 3, 4, 5, 6, 7];
+    this.colors = this.colors || [
+      "red",
+      "pink",
+      "purple",
+      "deep-purple",
+      "indigo",
+      "blue",
+      "light-blue",
+      "cyan",
+      "teal",
+      "green",
+      "light-green",
+      "lime",
+      "yellow",
+      "amber",
+      "orange",
+      "deep-orange",
+      "brown",
+      "grey",
+      "blue-grey"
+    ];
+  },
+  habitName: "",
+  habitFrequency: null,
+  habitColor: null,
   actions: {
     addHabit(habit) {
-      console.log(habit);
+      const habitName = this.get("habitName");
+      const freq = this.get("habitFrequency");
+      console.log(habitName, freq);
     },
     cancel() {
       this.onClose();
